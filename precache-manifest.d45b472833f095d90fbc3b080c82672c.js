bc63ca502f8fc3a3c69db9e3a6b6e50291ccbe00
Andrii Kodola <ankodola@gmail.com>
2019-04-09 23:02:21 +0200

    Updates

---
+++ precache-manifest.d45b472833f095d90fbc3b080c82672c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f07b79a8238f6484db5e",
+    "url": "/Treehouse_Techdegree_FSJS_Unit07/static/js/runtime~main.4cb07804.js"
+  },
+  {
+    "revision": "b20f1978f2a8d3751241",
+    "url": "/Treehouse_Techdegree_FSJS_Unit07/static/js/main.9e1d673d.chunk.js"
+  },
+  {
+    "revision": "28f772390365563f0663",
+    "url": "/Treehouse_Techdegree_FSJS_Unit07/static/js/2.bbee214d.chunk.js"
+  },
+  {
+    "revision": "b20f1978f2a8d3751241",
+    "url": "/Treehouse_Techdegree_FSJS_Unit07/static/css/main.d26140d7.chunk.css"
+  },
+  {
+    "revision": "1245f746dd5ca888fedf9a4f2c07c8d2",
+    "url": "/Treehouse_Techdegree_FSJS_Unit07/index.html"
+  }
+];
